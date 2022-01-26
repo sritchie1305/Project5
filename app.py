2eb992832c7fd8474fd0e7999b61d7cebfe6e48f
Steven Ritchie <steven.ritchie@natwest.com>
2022-01-26 22:01:42 +0000

    [skip ci] correct spelling mistake in app

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Cloud DevOps Engineer Capstgone Project by Steven"
+    return "Cloud DevOps Engineer Capstone Project by Steven"
 
 app.run(host="0.0.0.0", port=80)
